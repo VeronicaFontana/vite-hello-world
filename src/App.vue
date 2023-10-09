454282ed009041f3835d9a20d62c8531d8ce81f5
VeronicaFontana <fontana.veronica98@gmail.com>
2023-10-09 14:03:51 +0200

    [ADD] App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  name: "App.vue",
+  components:{
+        AppTitle
+      },
+
+  data(){
+    return{
+      titolo: "App con Vue"
+    }
+  }
+}
+</script>
+
+<template>
+  <AppTitle />
+  <h1>{{ titolo }}</h1>
+  
+</template>
+
+<style>
+
+</style>
